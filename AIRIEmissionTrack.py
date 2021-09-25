f02832994ce86420182689ba301ef22f848b9983
vladimir-laz <lazarev.vd@phystech.edu>
2021-09-25 15:52:26 +0300

    changed filename and added use example

--- AIRIEmissionTrack.py
+++ AIRIEmissionTrack.py
@@ -4,12 +4,31 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from tools import *
 
 EMISSION_PER_KWT = 511.7942
-
+FROM_mWATT_TO_KWATTH = 1000*1000*3600
 
 class AIRITracker:
     """
     in order to right gpu power consumption calculation you'd better to create
     EmissionTracker before any gpu uses
+
+    Use example:
+
+    import AIRIEmissionTrack.AIRITracker
+    
+    tracker = AIRITracker(project_name=your_project_name,
+                          experiment_description=your_experiment_description,
+                          save_file_name="you_file_name",
+                          measure_period=2,   #measurement will be done every 2 seconds
+                          emission_level=your_value   #kg/kWTh
+                          base_power=your_gou_base_power   #power of not working gpu
+                          )
+    
+    tracker.start()
+
+    *your gpu calculations*
+
+    tracker.stop()
+    
     """
     def __init__(self,
                  project_name,
@@ -22,51 +41,50 @@ class AIRITracker:
         self.project_name = project_name
         self.experiment_description = experiment_description
         self.save_file_name = save_file_name
-        if measure_period <= 0:
+        if (type(measure_period) == int or type(measure_period) == int) and measure_period <= 0:
             raise ValueError("measure_period should be positive number")
         self.measure_period = measure_period
         self.emission_level = emission_level
         self.base_power_consumption = base_power if base_power else gpu_power()
-        self.start = None
+        self.start_time = None
         self._scheduler = BackgroundScheduler()
         self._consumption = 0
 
     def _write_to_csv(self):
-        duration = self.stop - self.start
+        duration = time.time() - self.start_time
         emissions = self._consumption * duration
-        if not os.path.is_file(self.save_file_name):
+        if not os.path.isfile(self.save_file_name):
             with open(self.save_file_name, 'w') as file:
                 file.write("project_name,experiment_description,time,power_consumption,CO2_emissions\n")
-                file.write(f"{self.project_name},{self.experiment_description},{time},{self._consumption},{emissions}")
+                file.write(f"{self.project_name},{self.experiment_description},{duration},{self._consumption},{emissions}")
         else:
             with open(self.save_file_name, "a") as file:
-                file.write(f"{self.project_name},{self.experiment_description},{time},{self._consumption},{emissions}\n")
+                file.write(f"{self.project_name},{self.experiment_description},{duration},{self._consumption},{emissions}\n")
 
-    def func_for_sched(self):
+    def _func_for_sched(self):
         current_powers = gpu_power()
-        duration = time.time - self.start
+        print(self.start_time, time.time())
+        duration = time.time() - self.start_time
         for base_power, current_power in zip(self.base_power_consumption, current_powers):
-            self._consumption += (current_power - base_power) * duration
+            self._consumption += (current_power - base_power) / FROM_mWATT_TO_KWATTH * duration
         
-        print(self._consumption)
+        print("self._func_for_sched's consumption = ", self._consumption)
 
     def start(self):
-        self.start = time.time()
+        self.start_time = time.time()
         self.base_power_consumption = gpu_power()
         if self.measure_period is not None:
-            self._scheduler.add_job(self.func_for_sched, "interval", seconds=self.measure_period)
+            # print("scheduler was activated")
+            self._scheduler.add_job(self._func_for_sched, "interval", seconds=self.measure_period)
             self._scheduler.start()
 
     def stop(self, ):
-        if self.start is None:
+        if self.start_time is None:
             raise Exception("Need to first start the tracker by tracker.start()")
-        self.func_for_sched()
+        # print("self._stop was run")
+        self._func_for_sched()
         if self.measure_period is not None:
             self._scheduler.shutdown()
         self._write_to_csv()
-        print(self._consumption)
 
 
-
-print(time.time())
-print(is_gpu_available())
