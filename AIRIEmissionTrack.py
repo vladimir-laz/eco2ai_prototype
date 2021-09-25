2aa189367c0e22a50c3ff04fefe0603a258c9958
vladimir-laz <lazarev.vd@phystech.edu>
2021-09-25 16:39:28 +0300

    bax fixed

--- AIRIEmissionTrack.py
+++ AIRIEmissionTrack.py
@@ -23,8 +23,8 @@ class AIRITracker:
                           experiment_description=your_experiment_description,
                           save_file_name="you_file_name",
                           measure_period=2,   #measurement will be done every 2 seconds
-                          emission_level=your_value   #kg/kWTh
-                          base_power=your_gou_base_power   #power of not working gpu
+                          emission_level=your_value   #kg/MWTh
+                          base_power=your_gpu_base_power   #power of not working gpu
                           )
     tracker.start()
     *your gpu calculations*
