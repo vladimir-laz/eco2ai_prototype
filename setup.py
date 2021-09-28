ce8770ca7fa94e372a1cdfc993736a1340a4554b
vladimir-laz <lazarev.vd@phystech.edu>
2021-09-28 10:19:44 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,12 +13,9 @@ DEPENDENCIES = [
 setup(
     name = 'SberEmissionTracker',
     version = '0.1.0',
-    url = 'https://github.com/vladimir-laz/AIRIEmisisonTracker.git',
+    author="Vladimir Lazarev",
+    # url = 'https://github.com/vladimir-laz/AIRIEmisisonTracker.git',
     description = long_description,
     packages = find_packages(),
     install_requires=DEPENDENCIES,
-    # install_requires = [
-    #     # Github Private Repository
-    #     'ExampleRepo @ git+ssh://git@github.com/example_organization/ExampleRepo.git#egg=ExampleRepo-0.1'
-    # ]
 )
