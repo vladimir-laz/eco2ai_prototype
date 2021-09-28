1a83166219a75789f99c9a2c88d4cfeb78cb0e9d
vladimir-laz <lazarev.vd@phystech.edu>
2021-09-28 10:13:27 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+DEPENDENCIES = [
+    "APScheduler",
+    "pynvml",
+    "psutil",
+    "py-cpuinfo",
+]
+
 setup(
     name = 'SberEmissionTracker',
     version = '0.1.0',
     url = 'https://github.com/vladimir-laz/AIRIEmisisonTracker.git',
-    description = 'Tracks emission of CO2',
+    description = long_description,
     packages = find_packages(),
+    install_requires=DEPENDENCIES,
     # install_requires = [
     #     # Github Private Repository
     #     'ExampleRepo @ git+ssh://git@github.com/example_organization/ExampleRepo.git#egg=ExampleRepo-0.1'
