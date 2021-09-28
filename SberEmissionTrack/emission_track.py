e3f566d52dc465e578382f07092ec22975f92055
vladimir-laz <lazarev.vd@phystech.edu>
2021-09-28 12:48:15 +0300

    Update emission_track.py

--- SberEmissionTrack/emission_track.py
+++ SberEmissionTrack/emission_track.py
@@ -23,8 +23,6 @@ class Tracker:
                       experiment_description=your_experiment_description,
                       save_file_name="you_file_name",
                       measure_period=2,   #measurement will be done every 2 seconds
-                      emission_level=your_value   #kg/MWTh
-                      base_power=your_gpu_base_power   #power of not working gpu
                       )
     tracker.start()
     *your gpu calculations*
