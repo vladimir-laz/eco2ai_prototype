d02523881c7508a2cef23fb11b280985e1dc0875
vladimir-laz <lazarev.vd@phystech.edu>
2021-09-29 11:31:23 +0300

    Update tools_cpu.py

--- SberEmissionTrack/tools/tools_cpu.py
+++ SberEmissionTrack/tools/tools_cpu.py
@@ -1,5 +1,6 @@
 from cpuinfo import get_cpu_info
 import psutil
+import time
 
 # cpu benchmarks:
 # https://www.notebookcheck.net/Mobile-Processors-Benchmark-List.2436.0.html?
@@ -11,36 +12,45 @@ class CPU():
     1) All methods are done on the assumption that all cpus are equal
     2) it takes time to calculate base percent usage, - 10 seconds
     '''
-    def __init__(self, tdp=None):
+    def __init__(self, tdp=None, measure_period=0.5):
         self._cpu_dict = get_cpu_info()
+        self._measure_period = measure_period
         self.name = self._cpu_dict["brand_raw"]
 
         if tdp is None:
-            self.tdp = self._get_cpu_tdp()
+            self.tdp = self._set_cpu_tdp()
         else:
             self.tdp = tdp
-        self.num_cpu = self._cpu_dict["count"]
-        self.consumption = 0
-        self.base_persent_usage = self._calculate_base_percent_usage()
+        # self._num_cpu = self._cpu_dict["count"]
+        self._consumption = 0
+        self._base_persent_usage = self._calculate_base_percent_usage()
+        self._start = time.time()
 
-    def _get_cpu_tdp(self):
+    def get_consumption(self):
+        return self._consumption
+
+    def get_base_consumption(self):
+        return self._base_persent_usage
+
+    def _set_cpu_tdp(self):
         # prints to user cpu model and expect from him(her) to input cpu tdp
         tdp = float(input(f"Name of your cpu: {self.name}.\nPlease, enter it's TDP: "))
         return tdp
 
     def _calculate_base_percent_usage(self):
         percents = []
-        for _ in range(20): #calculating base percent usage for 10 sec
+        for _ in range(10): #calculating base percent usage for 10*measure_period sec
              percents.append(self.get_cpu_percent())
-        return sum(percents) / 20
-        
+        return sum(percents) / 10
 
     def get_cpu_percent(self):
-        percent = sum(psutil.cpu_percent(interval=0.5, percpu=True))
+        percent = sum(psutil.cpu_percent(interval=self.measure_period, percpu=True))
         return percent
 
-    def get_consumption(self, time_period):
-        consumption = self.tdp * (self.get_cpu_percent - self.base_persent_usage) / 100 * time_period
+    def calculate_consumption(self):
+        time_period = time.time() - self._start
+        self._start = time.time()
+        consumption = self.tdp * (self.get_cpu_percent - self._base_persent_usage) / 100 * (time_period + self._measure_period)
         self.consumption += consumption
         return consumption
 
