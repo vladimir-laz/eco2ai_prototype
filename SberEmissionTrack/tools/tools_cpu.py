e3779335a19f11034b523719e19a33fdafa883ad
vladimir-laz <lazarev.vd@phystech.edu>
2021-11-07 18:11:53 +0300

    added cpu database

--- SberEmissionTrack/tools/tools_cpu.py
+++ SberEmissionTrack/tools/tools_cpu.py
@@ -56,7 +56,7 @@ class CPU():
     def calculate_consumption(self):
         time_period = time.time() - self._start
         self._start = time.time()
-        consumption = self._tdp * (self.get_cpu_percent() - self._base_persent_usage) / 100 * (time_period + self._measure_period) / FROM_WATTs_TO_kWATTh
+        consumption = self._tdp * (self.get_cpu_percent()) / 100 * (time_period + self._measure_period) / FROM_WATTs_TO_kWATTh
         if consumption < 0:
             consumption = 0
         self._consumption += consumption
